7e8a6f8a7f4d6527101fe1dcde0fe2b1d8c1ba14
Antal Sergiu-Alexandru <45335304+quavoatl@users.noreply.github.com>
2019-03-09 13:55:11 +0200

    Update Main.java

--- Print missing numbers in Array - Using BitSet/Main.java
+++ Print missing numbers in Array - Using BitSet/Main.java
@@ -1,29 +1,43 @@
 package com.example;
 
+
 import java.util.BitSet;
 
 public class Main {
 
     public static void main(String[] args) {
-    int [] array = {1,3,5,6,7};
-    printMissingNumbers(array,7);
+    int[] testThis = {1,3,5,7,8,10};
+        checkNumbers(testThis,10);
 
     }
 
-    public static void printMissingNumbers(int [] array, int totalSize) {
+
+    public static void checkNumbers(int[] array,int totalSize) {
         int totalMissingNumbers = totalSize - array.length;
-        BitSet bitset = new BitSet(totalSize);
+        BitSet bitSet = new BitSet(totalSize);
+        // 1   3   5   7 8   10
+        // 0 0 0 0 0 0 0 0 0 0
+        // first loop i = 1 // add in bitset at index i-1(0)
+        // 1 0 0 0 0 0 0 0 0 0
+        // second loop i = 3// add in bitset at index i-1(2)
+        // 1 0 1 0 0 0 0 0 0 0
+        //....................
+        // 1 0 1 0 1 0 1 1 0 10
 
-        for(int number : array) {
-            bitset.set(number-1);
+        // 0 1 2 3 4 5 6 7 8 9
+
+
+        for (int i : array) {
+            bitSet.set(i - 1);
         }
 
-        int lastMissingNumberIndex = 0;
-        for(int i = 0; i < totalMissingNumbers;i++) {
-            lastMissingNumberIndex = bitset.nextClearBit(lastMissingNumberIndex);
-            lastMissingNumberIndex++;
-            System.out.println("Found missing number in your array : " + lastMissingNumberIndex);
-        }
+        int lastNumberMissing = 0;
+        for (int j = 0; j < totalMissingNumbers; j++) {
+            lastNumberMissing = bitSet.nextClearBit(lastNumberMissing);
+            //nextClearBit returns the index of the next 0 in bitset
 
+            lastNumberMissing++;
+            System.out.println("Found missing number : " + lastNumberMissing);
+        }
     }
 }
